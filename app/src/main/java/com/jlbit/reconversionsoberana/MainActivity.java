4cea752607e7aaf206dc6ee321fd32407112ec75
johanQ10 <johandym50@gmail.com>
2022-09-02 03:15:58 -0400

    Change to library

--- app/src/main/java/com/jlbit/reconversionsoberana/MainActivity.java
+++ app/src/main/java/com/jlbit/reconversionsoberana/MainActivity.java
@@ -4,19 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.DecimalFormat;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView image_star;
